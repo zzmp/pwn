dc948eee44e17eebafc5db1f8465257d25dd5680
zzmp <zmp@umich.edu>
2014-05-08 16:24:08 -0700

    Created pown.js/pown.coffee

---
+++ pown.coffee
@@ -0,0 +1,84 @@
+###
+ # # POWN eventing system - now in CoffeeScript!
+ # ### A 100 SLOC mangling of the <s>JavaScript</s> eventing system
+ #
+ # `pown` your objects using `obj.prototype = Object.create pown`
+ # That's right - don't even worry about your constructor - that's
+ # some pseudo-classical garbage. You *powned* it.
+ #
+ # Your object can now `pown` that minimalist MVC you got going:
+ #  obj.on event, callback, context  - *powned*
+ #  obj.off event, callback, context - *powned*
+ #  obj.off event, callback          - *powned*
+ #  obj.off event                    - *powned*
+ #  obj.trigger event, options       - *powned*
+ #  obj.set property, value          - *powned*
+ #  obj.get property, value          - *powned*
+###
+
+define ->
+  # pown that prototype
+  pown = {}
+
+  pown.on = (event, callback, context) ->
+    listener =
+      callback: callback
+      context: context or @
+    events = @_events or @_events = {}
+    (events[event] or events[event] = []).push listener
+
+    return @
+
+  pown.off = (event, callback, context) ->
+    return @ if not @_events or not @_events[event]
+
+    oList = @_events[event]
+    list = @_events[event] = []
+
+    switch arguments.length
+      when 3, 2 then
+        listener =
+          callback: callback
+          context: context or @
+        for oListener in oList
+          list.push oListener if
+            (oList.callback is not listener.callback) or
+            not (not context? or context is listener.context)
+      when 0 then
+        @_events = {}
+
+    return @
+
+  pown.trigger = (event, options) ->
+    return @ if not this@_events or not @_events[event]
+
+    list = @_events[event]
+    l = list.length
+
+    
+    for listener in [0...l]
+      listener.callback.apply listener.context, [@].concat options
+
+    return @
+
+  pown.set = (prop, val) ->
+    props = @props or @props = {}
+    oVal = props[prop]
+
+    props[prop] = val
+    this.trigger 'change',
+      p: prop       # property
+      o: oval       # original value
+      w: undefined, # why not?
+      n: val        # new value
+
+    return @
+
+  pown.get = (prop) ->
+    props = @props or @props = {}
+
+    return props[prop]
+
+  return pown
+
+# 84 SLOC                            - *powned* (MIT License)
---
+++ pown.js
@@ -0,0 +1,100 @@
+/**
+ * # POWN eventing system
+ * ### A 100 SLOC mangling of the JavaScript eventing system
+ *
+ * `pown` your objects using `obj.prototype = Object.create(pown);`
+ * That's right - don't even worry about your constructor - that's
+ * some pseudo-classical garbage. You *powned* it.
+ *
+ * Your object can now `pown` that minimalist MVC you got going:
+ *  obj.on(event, callback, context)  - *powned*
+ *  obj.off(event, callback, context) - *powned*
+ *  obj.off(event, callback)          - *powned*
+ *  obj.off(event)                    - *powned*
+ *  obj.trigger(event, options)       - *powned*
+ *  obj.set(property, value)          - *powned*
+ *  obj.get(property, value)          - *powned*
+ */
+
+define(function () {
+  // pown that prototype
+  var pown = {};
+
+  pown.on = function (event, callback, context) {
+    var listener = {
+      callback: callback,
+      context: context || this
+    };
+    var events = this._events || (this._events = {});
+    (events[event] || events[event] = []).push(listener);
+
+    return this;
+  };
+
+  pown.off = function (event, callback, context) {
+    if (!this._events || !this._events[event]) return this;
+
+    var oList = this._events[event];
+    var list = this._events[event] = [];
+
+    switch (arguments.length) {
+      case 3:
+      case 2:
+        var listener = {
+          callback: callback,
+          context: context || this
+        }
+        for (var i = 0; i < oList.length; i++) {
+          if ( oList[i].callback !== listener.callback ||
+             !(context === undefined || context === listener.context) ) {
+            list.push(oList[i]);
+          }
+        }
+      case 1:
+        break;
+      case 0:
+        this._events = {};
+    }
+
+    return this;
+  };
+
+  pown.trigger = function (event, options) {
+    if (!this._events || !this._events[event]) return this;
+
+    var list = this._events[event];
+    var l = list.length;
+
+    for (var i = 0; i < l; i++) {
+      var listener = list[event][i];
+      listener.callback.apply(listener.context, [this].concat(options));
+    }
+
+    return this;
+  };
+
+  pown.set = function (prop, val) {
+    var props = this.props || (this.props = {});
+    var oVal = props[prop];
+
+    props[prop] = val;
+    this.trigger('change', {
+      p: prop,      // property
+      o: oVal,      // original value
+      w: undefined, // why not?
+      n: val        // new value
+    })
+
+    return this;
+  };
+
+  pown.get = function (prop) {
+    var props = this.props || (this.props = {});
+
+    return props[prop];
+  };
+
+  return pown;
+});
+
+// 100 SLOC                           - *powned* (MIT License)
